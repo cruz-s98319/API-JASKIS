ece5a3eac79e3a4964aa195157985600a80490ba
Samantha Cruz <cruz.s98319@gmail.com>
2024-04-04 15:33:57 -0700

    Managing the database

--- commands.js
+++ commands.js
@@ -85,18 +85,27 @@
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
+//db.bounties.find(location:"Grasslands")
 
 // 2. Query for all bounties with a reward worth 10000 or more
+//db.bounties.find({reward:{$gte:10000}})
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
+//db.bounties.find({},{name:1, species:1, location:1,wantedFor:1, reward:1, captured:1})
 
 // 4. Query for a Groundhog in the Woodlands
+//db.bounties.find({$and:[{species:"Groundhog"}, {location:"Woodlands"}]})
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+//db.bounties.updateOne({_id:ObjectId('65ffcab50d22444c569b5ec1')}, {$set:{reward:10000}})
 
 // 2. Remove Lokinkajou
+//db.bounties.deleteOne({_id: ObjectId('65ffcab50d22444c569b5ebf')})
 
 // 3. Delete all bounties sent by Songbird
+//db.bounties.deleteMany({client:"Songbird"})
+
 
 // 4. Update all captured statuses to true
+//db.bounties.updateMany({captured:false}, {$set:{captured:true}})
